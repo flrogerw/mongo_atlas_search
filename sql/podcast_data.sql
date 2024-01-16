d58cf85c78d8084e140cb04193c4df6061f018c3
Raymond DeRoo <raymond.deroo@audacy.com>
2024-01-16 04:15:49 +0000

    Add files for Postgres

---
+++ sql/podcast_data.sql
@@ -0,0 +1,21 @@
+-- vim: set ts=4 expandtab
+SET client_min_messages TO WARNING;
+-- SET ROLE=:XXXX_admin;
+
+-- DROP SCHEMA IF EXISTS podcast CASCADE;
+-- CREATE SCHEMA podcast;
+
+SET search_path TO podcast;
+
+
+INSERT INTO struct_type (struct_type_id, date_created, date_modified, display_order, group_name, att_pub_ident, att_value)
+VALUES
+	  (110, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 10, 'desc_select',	'110',	'Cleaned')
+	, (120, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 20, 'desc_select',	'120',	'ChatGPT')
+	, (210, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 10, 'explicit',		'0',	'False')
+	, (220, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 20, 'explicit',		'1',	'True')
+	, (230, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 30, 'explicit',		'2',	'Indeterminate')
+	, (310, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 10, 'index_status',	'0',	'Auto')
+	, (320, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 20, 'index_status',	'1',	'Manual')
+	, (330, '2024-01-01 01:23:45', '2024-01-01 01:23:45', 30, 'index_status',	'2',	'Excluded')
+;
